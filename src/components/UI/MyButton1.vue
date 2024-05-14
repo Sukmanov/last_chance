19d513f481396a76af107e39ffe4905f3476cfc6
Semyon Sukmanov <ssukmanoff06@mail.ru>
2024-05-15 02:32:08 +0300

    fix component:'my-button-1'

--- src/components/UI/MyButton1.vue
+++ src/components/UI/MyButton1.vue
@@ -1,11 +1,45 @@
-<script setup>
+<script>
+export default {
+  name: 'my-button-1',
 
+  props: {
+    textButton: {
+      type: String,
+      required: true,
+    }
+  }
+}
 </script>
 
 <template>
-  $END$
+  <button v-text="textButton" class="button1"></button>
 </template>
 
-<style scoped>
+<style>
+.button1 {
 
+  border: white solid 1px;
+  border-radius: 25px;
+
+  width: 200px;
+  height: 50px;
+
+  font-size: 20px;
+  font-weight: 600;
+
+  color: white;
+  background-color: transparent;
+
+  margin: 10px 10px 10px 10px;
+}
+
+.button1:hover {
+  border: #FFB800 solid 1px;
+  color: #FFB800;
+}
+
+.button1:active {
+  color: black;
+  background-color: #FFB800;
+}
 </style>
