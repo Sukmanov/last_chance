ebe074497194b2a362040140a7ee1f5964c82726
Semyon Sukmanov <ssukmanoff06@mail.ru>
2024-05-15 09:13:21 +0300

    finish component:'top-header'

---
+++ src/components/UI/TopHeader.vue
@@ -0,0 +1,47 @@
+<script>
+
+export default {
+  name: 'top-header',
+
+  methods: {
+    Search() {
+      console.log('Search func');
+    },
+    Login() {
+      console.log('Login func');
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div id="block">
+    <img src="../../../images/logo.png" alt="" id="image">
+    <my-button-2 :textButton="'Поиск'" @click="Search"></my-button-2>
+    <my-button-2 :textButton="'Войти'" @click="Login"></my-button-2>
+  </div>
+</template>
+
+<style>
+
+/*нужно разместить кнопки и картинку*/
+#block {
+  display: flex;
+  justify-content: space-between;
+
+
+  margin-left: auto;
+  margin-right: auto;
+  align-items: center;
+
+  width: 70%;
+  height: 80px;
+}
+
+#image {
+  width: 80px;
+  height: 80px;
+}
+
+</style>
