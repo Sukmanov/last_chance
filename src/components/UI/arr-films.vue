c94d60caae767ef75af9f3a92ddcf4d0cc26acd2
Semyon Sukmanov <ssukmanoff06@mail.ru>
2024-05-17 03:49:43 +0300

    finish component:'arr-films'

---
+++ src/components/UI/arr-films.vue
@@ -0,0 +1,31 @@
+<script>
+
+  export default {
+    name: "arr-films",
+
+    props: {
+      films2: {
+        type: Array,
+        required: true,
+      }
+    },
+
+
+  }
+</script>
+
+<template>
+  <div class="arr-films" v-for="film in films2">
+    <film
+      :filmName="film.name"
+      :filmDescription="film.description"
+      :filmRating="film.rating"
+      :film-img-path="film.img"
+    >
+    </film>
+  </div>
+</template>
+
+<style>
+
+</style>
