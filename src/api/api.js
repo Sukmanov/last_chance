a74fcd196e1ecbb519a26e7e3e7c698bb4d5284a
cherry-pdb <mark.v.nazarov@yandex.ru>
2024-05-26 04:57:31 +0300

    Added getting random movie

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,19 @@
-const URL = "http://localhost:8060";
+const URL = "http://localhost:5123";
+const axios = require('axios');
+
+export async function getRandomMovie() {
+    try {
+        const response = await axios.get("http://localhost:5123/api/movie");
+        const movies = response.data;
+        const index = Math.floor(Math.random() * movies.length);
+        return movies[index];
+    } catch (e) {
+        console.error(e);
+    }
+}
 
 export const sendLoginRequest = async (login, password) => {
-    const response = await
+    //const response = await
 }
 
 const sendApiRequest = async (path, method = "GET", body = "") => {
