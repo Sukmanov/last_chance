7cf4f1b43467a7e75c4338353b9f3a9dddd5a5a3
cherry-pdb <mark.v.nazarov@yandex.ru>
2024-05-26 04:57:10 +0300

    Added random button

--- src/components/pages/MovieCatalog.vue
+++ src/components/pages/MovieCatalog.vue
@@ -1,8 +1,15 @@
 <script>
   import axios from 'axios';
+  import MyButton1 from "@/components/UI/MyButton1.vue";
+  import {getRandomMovie} from "@/api/api";
+  import MoviePopupWindow from "@/components/UI/MoviePopupWindow.vue";
+
   export default {
+    components: {MoviePopupWindow, MyButton1},
     data() {
       return {
+        randomMovie: null,
+        showPopup: false,
         page: 1,
         limit: 4,
         films: [
@@ -21,6 +28,25 @@
         } catch (e) {
           alert(e.message)
         }
+      },
+      showPopupWindow() {
+        this.showPopup = true;
+      },
+      closePopupWindow() {
+        this.showPopup = false;
+      },
+      getRandom() {
+        getRandomMovie()
+            .then(movie => {
+              this.randomMovie = movie;
+            });
+      },
+      onClickRandom() {
+        this.getRandom();
+        this.showPopupWindow();
+      },
+      updateMovie(movie) {
+        this.randomMovie = movie;
       }
     }
   }
@@ -28,6 +54,12 @@
 
 <template>
   <top-header></top-header>
+  <my-button1 @click="onClickRandom" class="random-button" text-button="Сгенерировать случайный фильм"></my-button1>
+  <transition name="random">
+    <div v-if="showPopup" class="popup">
+      <MoviePopupWindow :movie="randomMovie" @update-movie="updateMovie"></MoviePopupWindow>
+    </div>
+  </transition>
   <arr-films :films2="films"></arr-films>
   <div id="bottom"><bottom-contacts ></bottom-contacts></div>
 </template>
@@ -35,5 +67,30 @@
 <style>
 #bottom {
   margin-top: 100px;
+}
+
+.random-button {
+  margin: 15px auto 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
+
+.popup-content {
+  background-color: #000;
+  padding: 20px;
+  border-radius: 5px;
 }
 </style>
