48650165c243a7c274afc00451cd561d5b151bcf
cherry-pdb <mark.v.nazarov@yandex.ru>
2024-05-29 11:49:31 +0300

    Added achievements from db

--- src/components/UI/AchievementList.vue
+++ src/components/UI/AchievementList.vue
@@ -1,9 +1,25 @@
 <script>
+  import {getChallenge} from "@/api/api";
+
   export default {
     name: 'achievement-list',
-
+    props: {
+      challenges: {
+        type: Array,
+        required: true
+      }
+    },
+    async created() {
+      if (this.challenges !== null) {
+        for (let i = 0; i < this.challenges?.length; i++) {
+          const userChallenge = await getChallenge(this.challenges[i].challengeId);
+          this.userChallenges.push(userChallenge);
+        }
+      }
+    },
     data() {
       return {
+        userChallenges: [],
         arguments: [
           {id: 1, name: 'Achievement 1', text: 'Achievement 1 text'},
           {id: 2, name: 'Achievement 2', text: 'Achievement 2 text'},
@@ -16,8 +32,8 @@
 
 <template>
   <div class="list">
-    <div class="achievement-list" v-for="achievement in arguments">
-      <achievement :Name="achievement.name" :Description="achievement.text"></achievement>
+    <div class="achievement-list" v-for="userChallenge in userChallenges" :key="userChallenge.id">
+      <achievement :achievement="userChallenge" id="challenge"></achievement>
     </div>
   </div>
 
--- src/components/UI/achievement.vue
+++ src/components/UI/achievement.vue
@@ -1,15 +1,57 @@
 <script>
+  import {ElProgress, ElButton, ElIcon, ElButtonGroup} from "element-plus";
+  import {ArrowLeft, ArrowLeftBold, ArrowRight, ArrowRightBold} from '@element-plus/icons-vue'
+
   export default {
     name: 'achievement',
+    components: {
+      ArrowRightBold,
+      ArrowLeftBold,
+      ArrowLeft,
+      ArrowRight,
+      ElProgress,
+      ElButton,
+      ElIcon,
+      ElButtonGroup,
+    },
 
     props: {
-      Name: {
-        type: String,
-        required: true,
+      achievement: null
+    },
+    data() {
+      return {
+        progress: 0,
+        step: 100 / this.achievement.goal
+      }
+    },
+    methods: {
+      increaseProgress() {
+        if (this.progress < 100) {
+          this.progress += this.step
+          if (this.progress > 100) {
+            this.progress = 100;
+          }
+        }
       },
-      Description: {
-        type: String,
-        required: true,
+      decreaseProgress() {
+        if (this.progress > 0) {
+          this.progress -= this.step
+          if (this.progress < 0) {
+            this.progress = 0;
+          }
+        }
+      },
+      formatPercentage(percentage) {
+        return percentage.toFixed(2) + '%';
+      },
+      customColorMethod (percentage) {
+        if (percentage < 30) {
+          return '#909399'
+        }
+        if (percentage < 70) {
+          return '#e6a23c'
+        }
+        return '#67c23a'
       }
     }
   }
@@ -19,16 +61,79 @@
 
 <template>
   <div class="achievement">
-    <span id="achievement-name">{{Name}}</span>
-    <p id="achievement-text">{{Description}}</p>
+    <span id="achievement-name">{{this.achievement?.task}}</span>
+    <p id="achievement-text">Опыт: {{this.achievement?.experience}}</p>
+
+    <div id="progress-container">
+      <el-progress
+          :percentage="progress"
+          :text-inside="true"
+          :stroke-width="26"
+          :color="customColorMethod"
+          :format="formatPercentage"
+          striped
+          striped-flow
+          id="progress-bar"
+      ></el-progress>
+
+      <div class="buttons-container">
+        <el-button-group class="buttons">
+          <el-button @click="decreaseProgress" id="button-progress" text-button="-">
+            <el-icon id="icon-arrow"><ArrowLeftBold /></el-icon>
+          </el-button>
+          <el-button @click="increaseProgress" id="button-progress">
+            <el-icon id="icon-arrow"><ArrowRightBold /></el-icon>
+          </el-button>
+        </el-button-group>
+      </div>
+
+    </div>
+
   </div>
 </template>
 
 <style>
+#progress-container {
+  margin: auto;
+}
+
+#progress-bar {
+  justify-content: center;
+  margin: auto;
+  min-width: 150px;
+  max-width: 150px;
+  color: #FFB800;
+}
+
+#icon-arrow {
+  fill: #FFB800;
+  color: #FFB800;
+}
+
+.buttons-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  margin-top: 15px;
+}
+
+.buttons {
+  display: flex;
+}
+
+#button-progress {
+  width: 40px;
+  height: 40px;
+  background-color: black;
+}
 
 .achievement {
   display: flex;
-  width: 180px;
+  max-width: 180px;
+  min-width: 180px;
+  max-height: 200px;
+  min-height: 200px;
   margin: 0 auto 30px;
   border: 1px solid white;
   border-radius: 20px;
@@ -38,7 +143,7 @@
 }
 
 #achievement-name {
-  font-size: 20px;
+  font-size: 18px;
   font-weight: 500;
   font-family: "Rubik", sans-serif;
   color: white;
